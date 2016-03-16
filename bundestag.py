3ee2d08f587ab1b4674956607547b4a92e3fbd0b
Felix Biessmann <felix.biessmann@tu-berlin.de>
2016-03-16 16:24:45 +0100

    Added party classifier for manifesto project data.

--- bundestag.py
+++ bundestag.py
@@ -11,6 +11,46 @@ TXT_DIR = os.path.join(DATA_PATH, 'txt')
 OUT_DIR = os.path.join(DATA_PATH, 'out')
 clf = classifier.Classifier(train=False)
 
+partyManifestoMap = {
+    'gruene':41113,
+    'cducsu':41521,
+    'fdp':41420,
+    'spd':41320,
+    'afd':41953,
+    'linke':41223,
+    'pirates':41952
+}
+
+bundestagParties = {
+    17:['gruene','cducsu','spd','fdp','linke'],
+    18:['gruene','cducsu','spd','linke']
+}
+
+def classify_speeches():
+    
+def plot_leftright():
+    import pylab as pl
+    predTs = {
+        'gruene':[],
+        'cducsu':[],
+        'spd':[],
+        'fdp':[],
+        'linke':[]
+    }
+    for f in glob.glob(OUT_DIR+'/17*predictions.json'):
+        pred = json.load(open(f))
+        for party in pred.keys():
+            if len(pred[party]['leftright'])>0:
+                predTs[party].append(pred[party]['leftright']['right'])
+        
+    pl.figure(figsize=(20,10))
+    for party in pred.keys():
+        predTs[party] = sp.stack(predTs[party],1)
+        pl.clf()
+        pl.plot(predTs[party].T)
+        pl.savefig(OUT_DIR+'/partyTimeseries_'+party+'.pdf')
+            
+
 def get_party_predictions():
     pred_init = {'leftright':{},'manifestocode':{}}
     pred = {
@@ -20,13 +60,16 @@ def get_party_predictions():
             'fdp': {'leftright':{},'manifestocode':{}},
             'linke': {'leftright':{},'manifestocode':{}}
             }
-    for f in glob.glob(OUT_DIR+'/17*-with-classification.json'):
+    for f in glob.glob(OUT_DIR+'/17*.json'):
         speeches = json.load(open(f))
         print "processing %d speeches in %s"%(len(speeches),f)
         for speech in speeches:
-            if speech['speaker_party'] is not None and pred.has_key(speech['speaker_party']):
+            if speech['type']=='speech' and \
+            speech['speaker_party'] is not None and \
+            pred.has_key(speech['speaker_party']):
+                prediction = clf.predict(speech['text'])
                 for prediction_type in pred_init.keys():
-                    for pr in speech['predictions'][prediction_type]:
+                    for pr in prediction[prediction_type]:
                         k = pr['label']
                         v = pr['prediction']
                         if not pred[speech['speaker_party']][prediction_type].has_key(k): 
@@ -38,7 +81,39 @@ def get_party_predictions():
             for pr in pred[party][prediction_type].keys():
                 pred[party][prediction_type][pr] = sp.percentile(pred[party][prediction_type][pr],[5, 25, 50, 75, 95]).tolist()
     json.dump(pred,open(OUT_DIR+'/predictions.json','wb'))
+
+def get_party_predictions_daily():
+    pred_init = {'leftright':{},'manifestocode':{}}
+    for f in glob.glob(OUT_DIR+'/17*.json'):
+        pred = {
+                'gruene': {'leftright':{},'manifestocode':{}},
+                'cducsu': {'leftright':{},'manifestocode':{}},
+                'spd': {'leftright':{},'manifestocode':{}},
+                'fdp': {'leftright':{},'manifestocode':{}},
+                'linke': {'leftright':{},'manifestocode':{}}
+                }
+        speeches = json.load(open(f))
+        print "processing %d speeches in %s"%(len(speeches),f)
+        for speech in speeches:
+            if speech['type']=='speech' and \
+            speech['speaker_party'] is not None and \
+            pred.has_key(speech['speaker_party']):
+                prediction = clf.predict(speech['text'])
+                for prediction_type in pred_init.keys():
+                    for pr in prediction[prediction_type]:
+                        k = pr['label']
+                        v = pr['prediction']
+                        if not pred[speech['speaker_party']][prediction_type].has_key(k): 
+                            pred[speech['speaker_party']][prediction_type][k] = [v]
+                        else:
+                            pred[speech['speaker_party']][prediction_type][k].append(v)
+        for party in pred.keys():
+            for prediction_type in pred[party].keys():
+                for pr in pred[party][prediction_type].keys():
+                    pred[party][prediction_type][pr] = sp.percentile(pred[party][prediction_type][pr],[5, 25, 50, 75, 95]).tolist()
+        json.dump(pred,open(f.replace(".json","-predictions.json"),'wb'))
     
+
 
 def classify_all_speeches():
     for f in glob.glob(OUT_DIR+'/*.json'):
---
+++ party_classifier.py
@@ -0,0 +1,115 @@
+# -*- coding: utf-8 -*-
+import cPickle
+from scipy import ones,hstack,arange,reshape,zeros,setdiff1d
+import json
+import os
+import glob
+from itertools import chain
+from sklearn.linear_model import LogisticRegression
+from sklearn.feature_extraction.text import CountVectorizer,TfidfTransformer
+import pandas as pd
+from sklearn.grid_search import GridSearchCV
+from sklearn.pipeline import Pipeline
+from sklearn import metrics
+
+partyManifestoMap = {
+    'gruene':41113,
+    'cducsu':41521,
+    'fdp':41420,
+    'spd':41320,
+    'afd':41953,
+    'linke':41223,
+    'pirates':41952
+}
+
+def nullprediction(parties=['linke','gruene','spd','cducsu']):
+    return dict([(k, 1.0/len(parties)) for k in parties])
+
+def get_raw_text(folder="data",parties=['linke','gruene','spd','cducsu']):
+    '''
+    Loads raw text and labels from manifestoproject csv files 
+    (Downloaded from https://visuals.manifesto-project.wzb.eu)
+    '''
+    partyIds = [str(partyManifestoMap[p]) for p in parties]
+    files = glob.glob(folder+"/[0-9]*_[0-9]*.csv")
+    files = filter(lambda x: x.split('/')[-1].split('_')[-1] not in partyIds,files)
+    return zip(*chain(*filter(None,map(csv2DataTuple,files))))
+
+def csv2DataTuple(f):
+    '''
+    Extracts list of tuples of (text,label) for each manifestoproject file
+    '''
+    df = pd.read_csv(f).dropna()
+    partyId = f.split('/')[-1].split('_')[0]
+    party = [k for (k,v) in partyManifestoMap.items() if str(v) == partyId]
+    return zip(df['content'].tolist(), party * len(df))
+
+
+class PartyClassifier:
+
+    def __init__(self,train=False):
+        '''
+        Creates a PartyClassifier object
+        if no model is found, or train is set True, a new classifier is learned
+
+        INPUT
+        folder  the root folder with the raw text data, where the model is stored
+        train   set True if you want to train 
+
+        '''
+        # if there is no classifier file or training is invoked
+        if (not os.path.isfile('party_classifier.pickle')) or train:
+            print 'Training party classifier'
+            self.train()
+        print 'Loading party classifier'
+        self.clf = cPickle.load(open('party_classifier.pickle'))
+
+    def predict(self,text):
+        '''
+        Uses scikit-learn Bag-of-Word extractor and classifier and
+        applies it to some text. 
+
+        INPUT
+        text    a string to assign to a manifestoproject label
+        
+        '''
+        if (not type(text) is list) & (len(text)<3): 
+            return nullPrediction()
+        # make it a list, if it is a string
+        if not type(text) is list: text = [text]
+        # remove digits
+        text = map(lambda y: filter(lambda x: not x.isdigit(),y),text)
+        # predict probabilities
+        probabilities = self.clf.predict_proba(text).flatten()
+        predictions = dict(zip(self.clf.steps[-1][1].classes_, probabilities.tolist()))
+        
+        # transform the predictions into json output
+        return predictions
+   
+    def train(self,folds = 2):
+        '''
+        trains a classifier on the bag of word vectors
+
+        INPUT
+        folds   number of cross-validation folds for model selection 
+
+        '''
+        try:
+            # load the data
+            data,labels = get_raw_text()
+        except:
+            print('Could not load text data file in\n')
+            raise
+        # the scikit learn pipeline for vectorizing, normalizing and classifying text 
+        text_clf = Pipeline([('vect', CountVectorizer()),
+                            ('tfidf', TfidfTransformer()),
+                            ('clf',LogisticRegression(class_weight='auto',dual=True))])
+        parameters = {'vect__ngram_range': [(1, 1)],\
+               'tfidf__use_idf': (True,False),\
+               'clf__C': (10.**arange(4,5,1.)).tolist()}  
+        # perform gridsearch to get the best regularizer
+        gs_clf = GridSearchCV(text_clf, parameters, cv=folds, n_jobs=-1,verbose=3)
+        gs_clf.fit(data,labels)
+        # dump classifier to pickle
+        cPickle.dump(gs_clf.best_estimator_,open('classifier.pickle','wb'),-1)
+
