7543efc3845b7f1c1dea4dcbf01dd5e19704cf16
Felix Biessmann <felix.biessmann@tu-berlin.de>
2016-10-23 04:59:39 +0200

    mean instead of sum ...

--- sentiment_experiments.py
+++ sentiment_experiments.py
@@ -1,5 +1,5 @@
 import pandas as pd
-from scipy import corrcoef,array
+from scipy import corrcoef,array,mean
 import os,re
 from polyglot.text import Text
 
@@ -11,21 +11,21 @@ SEATS = {'EPP':221,'ECR':70,'ALDE':67,'GUE':52,'EFDD':48,'NI':52}
 def compute_sentiment_text(text):
     sentiment = 0
     try:
-        sentiment = sum([sum([w.polarity for w in s.words]) for s in text.sentences])
+        sentiment = mean([mean([w.polarity for w in s.words]) for s in text.sentences])
     except:
         pass
     return sentiment 
 
 def compute_sentiment(fn=EUFN,download=False):
-    df = pd.read_csv(fn)]
+    df = pd.read_csv(fn)
     df = df[df.text.apply(type) == str]
     df.language = df.language.apply(lambda x: x[:2])
     #df = df[df.language.str.match(LANGREGEXP)]
     if download:
         [os.system("polyglot download sentiment2.%s"%lang) for lang in df.language.unique()]
     df['TextPoly'] = df.apply(lambda x:Text(x.text,hint_language_code=x.language),axis=1)
-    df['Sentiment'] = df.TextPoly.apply(compute_sentiment_text)
-    sentiment = {k:(df[df.pol_group==k].Sentiment.mean(),v) for k,v in SEATS.item   
+    df['Sentiment'] = df.apply(lambda x: compute_sentiment_text(x.TextPoly) if x.pol_group in SEATS.keys() else 0,axis=1)
+    sentiment = {k:(df[df.pol_group==k].Sentiment.mean(),v) for k,v in SEATS.items()}   
     s,v = zip(*sentiment.values())
     print("Correlation number of seats vs sentiment %0.2f"%corrcoef(s,v)[0,1])
 
