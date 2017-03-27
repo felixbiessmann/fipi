01f9e89342bf1276a77ca716304a1391c621b9ca
Felix Biessmann <felix.biessmann@tu-berlin.de>
2017-03-27 23:21:24 +0200

    Some formatting

--- brplots.py
+++ brplots.py
@@ -4,6 +4,7 @@ from bokeh import mpl
 from bokeh.plotting import output_file, show
 import pandas as pd
 from operator import itemgetter
+import numpy as np
 
 colors = ["purple","red","green","yellow","gray","blue"]
 
@@ -34,16 +35,21 @@ def plotAll(folder = "data/manifesto/", domain = "Economy"):
     for party,fn in partyFiles:
         print("Getting texts of party %s"%party)
         data = pd.read_csv(folder+fn)
-        nsamples = min(200,len(data))
+        nsamples = min(100,len(data))
         pred = getLeftRightForDomain(data.sample(nsamples).content,clf,domain)
         predictions += [(x[0],x[1],party) for x in pred]
 
     df = pd.DataFrame(predictions,columns=['rightPrediction','text','party'])
     sns.set_style("whitegrid")
 
-    ax = sns.violinplot(y="rightPrediction",x="party",
+    ax = sns.violinplot(x="rightPrediction",y="party",
                     data=df, palette=sns.color_palette(colors), split=True,
-                    scale="count", inner="stick")
+                    scale="count", inner="stick", saturation=0.4)
+    ax.xlim([0,1])
+    ax.xticks(np.arange(0,1,.1))
+    ax.xlabel("p(rechts)")
+    ax.ylabel("Partei")
+    ax.title(domain)
     output_file(folder+"violinPlot-%s.html"%domain)
 
     show(mpl.to_bokeh())
