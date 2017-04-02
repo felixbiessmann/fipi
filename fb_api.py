fc090496b833eda41e9d00a61d35498937855a46
Felix Biessmann <felix.biessmann@tu-berlin.de>
2017-04-02 22:43:44 +0200

    FB quiz api

---
+++ fb_api.py
@@ -0,0 +1,11 @@
+
+def getCredentials():
+  app_id = os.environ["FACEBOOK_APP_ID"]
+  app_secret = os.environ["FACEBOOK_SECRET"]
+  return app_id,app_secret
+
+def get_posts(party="CDU",app_id,app_secret):
+  token = fbapi.get_app_access_token(app_id, app_secret)
+  graph = facebook.GraphAPI(access_token=token)
+  party = graph.get_object(partyId)
+  return graph.get_object(party['id'], 'posts')
