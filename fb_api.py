a96c1a795f0513caf4e47faae29a7153b85e9406
Felix Biessmann <felix.biessmann@tu-berlin.de>
2017-04-02 23:04:21 +0200

    with fbapi

--- fb_api.py
+++ fb_api.py
@@ -1,11 +1,13 @@
+import facebook
 
 def getCredentials():
-  app_id = os.environ["FACEBOOK_APP_ID"]
-  app_secret = os.environ["FACEBOOK_SECRET"]
-  return app_id,app_secret
+    app_id = os.environ["FACEBOOK_APP_ID"]
+    app_secret = os.environ["FACEBOOK_SECRET"]
+    return app_id,app_secret
 
 def get_posts(party="CDU",app_id,app_secret):
-  token = fbapi.get_app_access_token(app_id, app_secret)
-  graph = facebook.GraphAPI(access_token=token)
-  party = graph.get_object(partyId)
-  return graph.get_object(party['id'], 'posts')
+    fbapi = facebook.GraphAPI()
+    token = fbapi.get_app_access_token(app_id, app_secret)
+    graph = facebook.GraphAPI(access_token=token)
+    party = graph.get_object(partyId)
+    return graph.get_object(party['id'], 'posts')
